3227a60972439310a981a0efbe7cc07090089fa7
OlyaYankovskaya <oyankovskaya8@gmail.com>
2023-12-10 00:19:38 +0300

    storing-values-with-the-assignment-operator.js

--- JavaScript Algorithms and Data Structures/comment-your-javascript-code.js
+++ JavaScript Algorithms and Data Structures/comment-your-javascript-code.js
@@ -1,5 +1,5 @@
 //DESCRIPTION:
-//Comments are lines of code that JavaScript will intentionally ignore. Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
+/*Comments are lines of code that JavaScript will intentionally ignore. Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
 
 //    There are two ways to write comments in JavaScript:
 
